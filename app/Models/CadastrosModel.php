8b1c1b7fe522aaa65984b2be1d626b0b20f733eb
fhfrosacruz <fhfrosacruz@hotmail.com>
2022-11-28 13:40:40 -0300

    migrate funcionando e bootstrap

---
+++ app/Models/CadastrosModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CadastrosModel extends Model
+{
+    //use HasFactory;
+    protected $table = 'cadastro'; //nome da tabela que vai ser criada quando rodar o migration
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    teste
-</body>
-</html>
+@extends('templates.default')
+
+@section('inicial')
+    <h1 class="text-center">Crud Laravel</h1>
+    <hr>
+@endsection
